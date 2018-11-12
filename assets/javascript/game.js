04a1551fcdf6aee3def96d2f2a61946dec332757
JeromeHD <jaymagolan@gmail.com>
2018-11-11 22:18:23 -0500

    update2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,4 +6,12 @@ var totalScore = 0;
 
 document.onkeyup = function() {
 
+    // random number 
+$("#random-button").on("click", function() {
+    var random = Math.floor(Math.random() * 1000) + 1;
+    $("#random-number").text(random);
+});
+
+
+
 };
